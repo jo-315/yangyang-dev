c37794a4e0aad5723148094905f9e6ea6fa7a646
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-12-12 21:38:53 +0900

    header-nav

---
+++ yangyang-theme/template-parts/header-navigation.php
@@ -0,0 +1,32 @@
+<?php
+// ヘッダーのナビゲーション
+?>
+<div class="header-top-wrap">
+  <div class="header-logo-wrap">
+    <a href="/">
+      <img
+        src="<?php echo get_stylesheet_directory_uri(); ?>/img/header-logo.png"
+        class="header-logo"
+      >
+    </a>
+  </div>
+
+  <div class='header-title-wrap'>
+    <span class="header-title">
+      やんやんとやんやん
+    </span>
+  </div>
+
+  <div class="header-main-navigation-wrap">
+    <nav class="header-main-navigation">
+      <ul class='header-main-navigation-items-wrap'>
+        <li class='header-main-navigation-item'>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+  <div class="header-nav-button">
+    <button></button>
+  </div>
+</div>
