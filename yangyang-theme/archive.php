86c88c3c38e413a9daeb19ccdcdaa4cb5e023def
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-12-16 14:42:58 +0900

    arcjove

--- yangyang-theme/archive.php
+++ yangyang-theme/archive.php
@@ -1,52 +1,58 @@
 <?php
-/**
- * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package yangyang
- */
-
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
+<div class="content-area">
+	<div class="content-wrap">
+
+		<?php get_template_part( 'template-parts/content', 'navigation' ); ?>
+
 		<main id="main" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+			<div class="site-main-title-wrap archive-main-title-wrap">
+				<div class="site-main-title">
+					<?php
+					the_archive_title( '<h2 class="category-page-title">', '</h2>' );
+					?>
+				</div>
+				<div class="site-main-title--img-wrap">
+					<img
+						src="<?php echo get_stylesheet_directory_uri(); ?>/img/index-article-title.png"
+						class="site-main-title--img"
+					/>
+				</div>
+			</div>
 
-			<header class="page-header">
+			<div class="site-main-articles-wrap">
+
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+				if ( have_posts() ) :
+
+					while ( have_posts() ) :
+						the_post();
+
+						get_template_part( 'template-parts/content', get_post_type() );
+
+					endwhile;
+
+					the_posts_navigation();
+
+				else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
 				?>
-			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			</div>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+		</main>
 
-			endwhile;
+		<?php get_template_part( 'template-parts/content', 'sidebar' ); ?>
 
-			the_posts_navigation();
+	</div>
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</div>
 
 <?php
 get_footer();
