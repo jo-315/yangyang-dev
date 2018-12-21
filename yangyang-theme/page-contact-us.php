3682566f62b1aa9638609acc25330d0a82cf7769
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-12-21 20:55:07 +0900

    途中コミット

---
+++ yangyang-theme/page-contact-us.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+?>
+
+<div class="page_content_block">
+  <?php // echo do_shortcode( '[contact-form-7 id="54" title="コンタクトフォーム_default"]' ); ?>
+  <?php echo do_shortcode( '[contact-form-7 id="48" title="コンタクトフォーム 1"]' ); ?>
+</div>
+
+<?php
+get_footer();
---
+++ yangyang-theme/page-portfolio.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+
+<div class="portfolio">
+  ただいま準備中です！また遊びに来てください！
+</div>
+
+<?php
+get_footer();
