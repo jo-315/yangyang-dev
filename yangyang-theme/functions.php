c33be2f20f24c5e1e2f896cbfb93002bf615f9f4
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-12-29 19:23:28 +0900

    途中コミット

--- yangyang-theme/functions.php
+++ yangyang-theme/functions.php
@@ -366,3 +366,13 @@ function isBot() {
 	}
 	return $is_bot;
 }
+
+// add defer
+if (!(is_admin() )) {
+function add_defer_to_enqueue_script( $url ) {
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.min.js' ) ) return $url;
+    return "$url' defer charset='UTF-8";
+}
+add_filter( 'clean_url', 'add_defer_to_enqueue_script', 11, 1 );
+}
