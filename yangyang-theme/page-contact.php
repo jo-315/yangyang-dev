116d5b7dcc61ab06eba57f573c4e26751f56813b
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-12-29 11:05:31 +0900

    途中コミット

--- yangyang-theme/page-contact.php
+++ yangyang-theme/page-contact.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 
-<div class="page_content_block">
+<div class="page_content_block contact-page">
   <?php // echo do_shortcode( '[contact-form-7 id="11" title="コンタクトフォーム 1"]' ); ?>
   <?php echo do_shortcode( '[contact-form-7 id="48" title="コンタクトフォーム 1"]' ); ?>
 </div>
