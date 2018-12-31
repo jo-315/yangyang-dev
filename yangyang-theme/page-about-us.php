7b43c1b8054d114760f211b0c268f5e279ca2df0
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-12-31 09:40:26 +0900

    途中コミット

--- yangyang-theme/page-about-us.php
+++ yangyang-theme/page-about-us.php
@@ -2,9 +2,211 @@
 get_header();
 ?>
 
-<div class="portfolio">
-  ただいま準備中です！渾身の１ページを作るのでお楽しみに！
+<div class="aboutContainer">
+  <div class="aboutContainer_introduction">
+    <div class="aboutContainer_titleArea">
+      <p>
+        やんやんとやんやんって？
+      </p>
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/index_01.png" class="lazyload" alt="やんやんとやんやん"/>
+    </div>
+
+    <div class="aboutContainer_textArea">
+      <p>
+        やんやんとやんやんは、やんやんとじょーの日常を綴っているブログです。嬉しかったことや楽しかったこと、勉強したことを発信していきます。<br />
+        </br>
+        日常の隙間にのぞいていただけたらとっても嬉しいです。
+      </p>
+    </div>
+  </div>
+
+  <div class="aboutContents clear">
+    <div class="aboutBlock">
+      <div class="aboutBlock_profile">
+        <p>
+          プロフィール<span>profile</span>
+        </p>
+      </div>
+    </div>
+
+    <div class="about_Contents01 clear">
+      <div class="aboutBlock01 clear">
+        <div class="left aboutBlock01--item">
+          <div class="nameage aboutSp">
+            <p class="aboutTitle">
+              名前  年齢<span class="about-subTitle02 pink">name & age</span>
+            </p>
+
+            <p class="aboutText nameDeco">
+              <span class="jocolor">じょー</span>&nbsp;<span>20</span><span class="font-12">th</span>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jo.png" class="joicon lazyload"/>
+            </p>
+
+            <p class="aboutText nameDeco">
+              <span class="yancolor" >やんやん</span>&nbsp;<span>20</span><span class="font-12">th</span>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/yanyan.png" class="yaicon lazyload"/>
+            </p>
+          </div>
+
+          <div class="major aboutSp">
+            <p class="aboutTitle">
+              所属<span class="about-subTitle03 pink">belongs</span>
+            </p>
+
+            <p class="aboutText aboutText01">
+              <span class="jocolor">じょー</span><br />
+              大阪大学　工学部　環境・エネルギー工
+            </p>
+
+            <p class="aboutText">
+              <span class="yancolor" >やんやん</span><br />
+              大阪大学　人間科学部　行動学科
+            </p>
+          </div>
+        </div>
+
+        <div class="about-sns aboutSp">
+          <p class="aboutTitle">
+            Twitterアカウント<span class="about-subTitle03 pink">twitter</span>
+          </p>
+
+          <p class="aboutText aboutText01 left clear about-sns-item">
+            <span class="jocolor left">じょー</span>
+            <a
+              href="https://twitter.com/VYZ3pDOkmPiwXtf?lang=ja"
+              target="_blank"
+              class="sns-social-wrap profile-sns-social-wrap"
+            >
+              <img
+                src="<?php echo get_stylesheet_directory_uri(); ?>/img/Twitter_Social_Icon_Rounded_Square_Color.png"
+                alt="Twitter"
+                class="profile-sns-social-icon"
+              />
+            </a>
+          </p>
+
+          <p class="aboutText aboutText01 left clear about-sns-item">
+            <span class="yancolor left" >やんやん</span>
+            <a
+              href="https://twitter.com/b3OJjuMQqAvlmO7?lang=ja"
+              target="_blank"
+              class="sns-social-wrap profile-sns-social-wrap"
+            >
+              <img
+                src="<?php echo get_stylesheet_directory_uri(); ?>/img/Twitter_Social_Icon_Rounded_Square_Color.png"
+                alt="Twitter"
+                class="profile-sns-social-icon"
+              />
+            </a>
+          </p>
+        </div>
+
+        <div class="sports aboutSp clear">
+          <p class="aboutTitle aboutTitle02">
+            好きな本<span class="about-subTitle03 pink">book</span>
+          </p>
+
+          <p class="aboutText aboutText01 aboutText02-book left">
+            <span class="jocolor">じょー</span><br />・こころ&nbsp;夏目漱石
+          </p>
+
+          <p class="aboutText aboutText01 aboutText02-book left">
+            <span class="yancolor" >やんやん</span><br />・ノルウェイの森&nbsp;村上春樹
+          </p>
+        </div>
+      </div>
+
+    <div class="aboutBlock02 clear">
+      <div class="birthplace aboutSp">
+        <p class="aboutTitle ">
+          出身地<span class="about-subTitle02 blue">hometown</span>
+        </p>
+
+        <p class="aboutText aboutText01 left">
+          <span class="jocolor">じょー</span><br />・愛知県豊田市<br />地元のもんじゃ焼きを家族と食べるのが好き
+        </p>
+
+        <p class="aboutText left">
+          <span class="yancolor" >やんやん</span><br />・和歌山県<br />冬になると実家からみかんは絶対送られてくる
+        </p>
+      </div>
+
+      <div class="position aboutSp">
+        <p class="aboutTitle aboutTitle01">
+          経歴<span class="about-subTitle02 pink">position</span>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/index_02.png" class="positionIcon lazyload"/>
+        </p>
+
+        <p class="aboutText aboutText01 left">
+          <span class="jocolor">じょー</span><br />・Baseconnect.Inc エンジニア<br />・<a href="https://moire.xsrv.jp/" target="_blank">moire 社会貢献活動に繋ぐWebメディア</a> 代表<br />
+          ・阪大NICE代表 / west(関西NICE運営委員会) / 関西NICE学生チーム発足人
+        </p>
+
+        <p class="aboutText left">
+          <span class="yancolor" >やんやん</span><br />・現役大学生<br />・webサイト制作会社でインターン
+        </p>
+      </div>
+    </div>
+  </div>
+
+<div class="about_Contents02 clear">
+  <div class="aboutBlock03 clear">
+    <div class="character aboutSp">
+      <p class="aboutTitle ">
+        性格<span class="about-subTitle02 blue">character</span>
+      </p>
+
+      <p class="aboutText aboutText01 left">
+        <span class="jocolor">じょー</span><br />好きなタイプは戸田恵梨香。<br />自分のこと話すのが苦手なタイプのコミュ障。基本は個人主義だが、意外と甘えん坊。泣き顔に定評がある(らしい...)。
+      </p>
+
+      <p class="aboutText left">
+        <span class="yancolor" >やんやん</span><br />ディカプリオ大好きで二重まぶたが大好きなのでじょーはもちろん二重。<br />ゲラなので基本的に笑ってるけど、愛想笑いの時は「たっ」とか「ふっ」のように一単語で笑う。
+        無表情の時怖い　
+      </p>
+    </div>
+  </div>
+
+  <div class="aboutBlock04 clear">
+    <div class="hobby aboutSp clear">
+      <p class="aboutTitle">
+        趣味<span class="about-subTitle03 pink">hobby</span>
+      </p>
+
+      <p class="aboutText aboutText01 aboutText02 left">
+        <span class="jocolor">じょー</span><br />趣味と仕事を分けない生き方をしたい。今はmoireとNICEでの活動が趣味と呼べるかもしれないし、ある意味仕事とも呼べるかもしれない。
+      </p>
+
+      <p class="aboutText aboutText02 aboutText03 left">
+        <span class="yancolor" >やんやん</span><br />ライブに行くのはかなり好き。ミスチル、ラッド、ワンオク、あいみょんが最近のお気に入り。<br />洋画　ライフオブデビッドゲイル　ギルバートグレイプはおすすめ
+      </p>
+    </div>
+
+    <div class="jo">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/index_06.png" class="hoverIcon lazyload"/>
+    </div>
+
+    <div class="purpose aboutSp">
+      <p class="aboutTitle">
+        将来の目標<span class="about-subTitle03 pink">purpose</span>
+      </p>
+
+      <p class="aboutText aboutText01 aboutText02 left">
+        <span class="jocolor">じょー</span><br />将来は核融合により、エネルギー問題にブレイクスルーをもたらしたい。その上で、ボランティアを始めとする、いわゆる社会貢献活動を活発にし、現状国・企業が手を伸ばせていない部分での改善を試みたい。
+      </p>
+
+      <p class="aboutText aboutText02 aboutText03 left">
+        <span class="yancolor" >やんやん</span><br />
+        最近、社会をちょっとよくするようなサービスとしてのデザイン・美しさとしてのなど、様々な形のデザインに興味を持ち始める。心理学分野ではニューロマーケティングに興味があり、研究したのちその方面の仕事につきたいな〜とも。
+      </p>
+    </div>
+  </div>
+
+  <div class="about-footerImage">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/index_05.png" class="lazyload"/>
+  </div>
 </div>
+
 
 <?php
 get_footer();
