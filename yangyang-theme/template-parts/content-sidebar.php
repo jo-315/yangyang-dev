b3be663c081a52a3e1749cddca057a9a9f8fb219
jo-315 <32921100+jo-315@users.noreply.github.com>
2019-01-05 13:22:31 +0900

    途中コミット

--- yangyang-theme/template-parts/content-sidebar.php
+++ yangyang-theme/template-parts/content-sidebar.php
@@ -1,5 +1,62 @@
 <aside class="sidebar-wrap">
 
+	<!-- 人気記事 -->
+	<div class="sidebar-content recommend-article">
+		<div class="sidebar-title">
+			人気記事
+		</div>
+		<ul>
+			<?php
+				$the_query = new WP_Query(array(
+					'post_status' => 'published',
+					'post_type' => 'post',
+					'orderby' => 'meta_value_num',
+					'meta_key' => '_liked',
+					'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1,
+					'posts_per_page'=> '3'
+				));
+			?>
+
+			<?php
+			  $count = 0;
+			  while($the_query->have_posts()):$the_query->the_post();
+
+				$count += 1;
+			?>
+
+				<?php
+					// $category = get_the_category();
+					// $cat_name = $category[0]->cat_name;
+				?>
+
+		 		<li class="article-wrap article-rank-wrap-<?php echo $count?>">
+		 			<a href="<?php the_permalink(); ?>" class="article-link sidebar-link">
+						<div class="article_rank_title article_rank_title-<?php echo $count?>">
+							<?php echo $count?>
+						</div>
+
+						<div class="sidebar-img">
+							<?php yangyang_theme_post_thumbnail(); ?>
+						</div>
+
+		 				<div class="sidebar-desc">
+
+		 					<div class="sidebar-item-title">
+		 						<?php the_title( '<span>', '</span>' ); ?>
+		 					</div>
+
+ 							<div class="sidebar-desc-author">
+ 								<?php yangyang_theme_posted_by(); ?>
+ 							</div>
+						</div>
+		 			</a>
+		 		</li>
+			<?php
+		    endwhile;
+			?>
+		</ul>
+	</div>
+
 	<!-- アクセスランキング -->
 	<div class="sidebar-content popular-article">
 		<div class="sidebar-title">
@@ -9,62 +66,66 @@
 				class="header-logo"
 			> -->
 		</div>
+		<ul>
 
-			<ul>
+			<?php if( is_single() && !is_user_logged_in() && !isBot() ): //個別記事 かつ ログインしていない かつ 非ボット
+				set_post_views(); //アクセスをカウントする
+			endif; ?>
 
-				<?php if( is_single() && !is_user_logged_in() && !isBot() ): //個別記事 かつ ログインしていない かつ 非ボット
-					set_post_views(); //アクセスをカウントする
-				endif; ?>
+			<?php
+				$args = array(
+					'post_type'     => 'post',
+					'numberposts'   => 4,       //表示数
+					'meta_key'      => 'pv_count',
+					'orderby'       => 'meta_value_num',
+					'order'         => 'DESC',
+				);
+				$posts = get_posts( $args );
+				if( $posts ):
+			?>
+
+			<?php
+			  $count = 0;
+			  foreach( $posts as $post ) : setup_postdata( $post );
+
+				$count += 1;
+			?>
 
 				<?php
-					$args = array(
-						'post_type'     => 'post',
-						'numberposts'   => 4,       //表示数
-						'meta_key'      => 'pv_count',
-						'orderby'       => 'meta_value_num',
-						'order'         => 'DESC',
-					);
-					$posts = get_posts( $args );
-					if( $posts ):
+					// $category = get_the_category();
+					// $cat_name = $category[0]->cat_name;
 				?>
 
-				<?php foreach( $posts as $post ) : setup_postdata( $post ); ?>
+		 		<li class="article-wrap article-rank-wrap-<?php echo $count?>">
+		 			<a href="<?php the_permalink(); ?>" class="article-link sidebar-link">
+						<div class="article_rank_title article_rank_title-<?php echo $count?>">
+							<?php echo $count?>
+						</div>
 
-					<?php
-						$category = get_the_category();
-						$cat_name = $category[0]->cat_name;
-					?>
+						<div class="sidebar-img">
+							<?php yangyang_theme_post_thumbnail(); ?>
+						</div>
 
-			 		<li class="article-wrap article-wrap-<?php echo $cat_name?>">
-			 			<a href="<?php the_permalink(); ?>" class="article-link sidebar-link">
-							<div class="sidebar-img">
-								<?php yangyang_theme_post_thumbnail(); ?>
-							</div>
+		 				<div class="sidebar-desc">
 
-			 				<div class="sidebar-desc">
+		 					<div class="sidebar-item-title">
+		 						<?php the_title( '<span>', '</span>' ); ?>
+		 					</div>
 
-			 					<div class="sidebar-item-title">
-			 						<?php the_title( '<span>', '</span>' ); ?>
-			 					</div>
+ 							<div class="sidebar-desc-author">
+ 								<?php yangyang_theme_posted_by(); ?>
+ 							</div>
+						</div>
+		 			</a>
+		 		</li>
+			<?php
+		    endforeach;
+				wp_reset_postdata();
+			?>
+			<?php else : ?>
+				<p>アクセスランキングはまだ集計されていません。</p>
+			<?php endif; ?>
 
-	 							<div class="sidebar-desc-author">
-	 								<?php yangyang_theme_posted_by(); ?>
-	 							</div>
-							</div>
-			 			</a>
-			 		</li>
-				<?php
-			    endforeach;
-					wp_reset_postdata();
-				?>
-				<?php else : ?>
-					<p>アクセスランキングはまだ集計されていません。</p>
-				<?php endif; ?>
-
-			</ul>
-		</div>
-
-	</div>
-	<div class="sidebar-content recommend-article">
+		</ul>
 	</div>
 </aside>
